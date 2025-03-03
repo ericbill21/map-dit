cb4b36a968b69d071b21250eb918b4e5e11e44a6
Eric Tillmann Bill <erbill@armin.inf.ethz.ch>
2025-03-03 11:10:36 +0100

    Added mp_relu :)

--- src/basic/mp_silu.py
+++ src/basic/mp_silu.py
@@ -1,7 +1,15 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
+import math
+
 
 class MPSiLU(nn.Module):
     def forward(self, x):
-        return F.silu(x) / 0.596
+        # return F.silu(x) / 0.596
+        return F.relu(x) * math.sqrt(2.0)
+
+    
+class MPReLU(nn.Module):
+    def forward(self, x):
+        return F.relu(x) * math.sqrt(2.0)
